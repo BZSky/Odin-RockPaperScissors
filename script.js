ff35540168fbd6b6ce8c10b59b3c0712de964a6f
Boris Zapolsky <boris.zapolsky@gmail.com>
2024-04-15 21:40:37 +0300

    Finish app logic

--- script.js
+++ script.js
@@ -1,51 +1,28 @@
 /*
-
 Constants
 1. Array of choices
 2. Array of round results
-
-Functions:
-Generate opponent's choice
-1. Get a random number 0 to 3
-2. Show opponent's choice
-
-
-Get player choice
-1. Prompt for player choice
-2. Evaluate & lowercase
-3. Player choice = indexOf()
-
-Compare choices
-1. If === then draw
-2. If 0 && 1 then 1 wins
-3. If 0 && 2 then 0 wins
-4. If 1 && 2 then 2 wins
-5. Show winner 
-6. Return winner
-
-Play 1 round of Rock Paper Scissors
-1. Call player choice
-2. Call computer choice
-3. Call compare choices
-4. Save round results
-
-Match function 5 rounds
-1. Loop round function 5 times
-2. Push results to an array
-3. Evaluate array to get winner
-4. Show winner
-5. Reset game
 */
-
 const CHOICES = ["rock", "paper", "scissors"];
 const RESULTS = ["player", "computer", "draw"];
 
+/*
+Generate opponent's choice
+1. Get a random number 0 to 3
+2. Show opponent's choice
+*/
 function getComputerChoice() {
   const opponentChoice = Math.floor(Math.random() * 3);
   console.log(`Computer chose ${CHOICES[opponentChoice]}`);
   return opponentChoice;
 }
 
+/*
+Get player choice
+1. Prompt for player choice
+2. Evaluate & lowercase
+3. Player choice = indexOf()
+*/
 function getPlayerChoice() {
   let playerChoice = prompt("Choose rock, paper or scissors");
   if (playerChoice) {
@@ -58,6 +35,15 @@ function getPlayerChoice() {
   return CHOICES.indexOf(playerChoice);
 }
 
+/*
+Compare choices
+1. If === then draw
+2. If 0 && 1 then 1 wins
+3. If 0 && 2 then 0 wins
+4. If 1 && 2 then 2 wins
+5. Show winner 
+6. Return winner
+*/
 function compareChoices(playerSelection, computerSelection) {
   let roundResult;
   if (playerSelection === computerSelection) {
@@ -77,8 +63,48 @@ function compareChoices(playerSelection, computerSelection) {
   return roundResult;
 }
 
+/*
+Play 1 round of Rock Paper Scissors
+1. Call player choice
+2. Call computer choice
+3. Call compare choices
+4. Save round results
+*/
 function playRound() {
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
   return compareChoices(playerChoice, computerChoice);
 }
+
+/*
+Match function 5 rounds
+0. declare results array
+1. Loop round function 5 times
+2. Push results to an array
+3. Evaluate array to get winner
+4. Show winner
+*/
+function playGame() {
+  const gameResults = [];
+  while (gameResults.length < 5) {
+    gameResults.push(playRound());
+  }
+  let result = gameResults.reduce(function (points, count) {
+    if (count in points) {
+      points[count]++;
+    } else {
+      points[count] = 1;
+    }
+    return points;
+  }, {});
+
+  if (result[1] == undefined || result[0] > result[1]) {
+    console.log(`${RESULTS[0]} wins!`);
+  } else if (result[0] == undefined || result[0] < result[1]) {
+    console.log(`${RESULTS[1]} wins!`);
+  } else {
+    console.log(`It's a draw!`);
+  }
+}
+
+playGame();
