75c8e712c122fd260839b59357cbf7a430707dc4
Boris Zapolsky <boris.zapolsky@gmail.com>
2024-04-15 16:17:41 +0300

    Initial commit
    Added a simple HTML and a script.js with an initial plan of app logic.

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+Functions:
+Generate opponent's choice
+1. Get a random number 0 to 3
+2. Assign a value of choices array = generated index
+
+Play 1 round of Rock Paper Scissors
+1. Get value from player
+2. Eval to available choices (& lowercase)
+3. Show opponent's choice
+
+Save round results
+1. Compare choices in round function return stmnt
+2. Push result to an array
+3. Show round result
+
+Match function 5 rounds
+1. Loop round function 5 times
+2. Evaluate array to get winner
+3. Show winner
+4. Reset game
+*/
