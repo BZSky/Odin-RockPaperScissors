d26406e2d7b740edd20322b3268766e547357cf5
Boris Zapolsky <boris.zapolsky@gmail.com>
2024-04-28 15:14:52 +0300

    Finish refactoring

--- script.js
+++ script.js
@@ -5,35 +5,30 @@ Constants
 */
 const CHOICES = ["rock", "paper", "scissors"];
 const RESULTS = ["player", "computer", "draw"];
+let gameResults = [0, 0];
 
 /*
 Generate opponent's choice
 1. Get a random number 0 to 3
-2. Show opponent's choice
+2. Display opponent's choice
+3. Return opponent's choice
 */
 function getComputerChoice() {
   const opponentChoice = Math.floor(Math.random() * 3);
-  console.log(`Computer chose ${CHOICES[opponentChoice]}`);
+  let computerChoiceDisplay = document.createElement("div");
+  computerChoiceDisplay.textContent = `Computer chose ${CHOICES[
+    opponentChoice
+  ].toUpperCase()}`;
+  choicesDisplay.appendChild(computerChoiceDisplay);
   return opponentChoice;
 }
 
 /*
-Get player choice
+Get player choice - DEPRECATED with UI
 1. Prompt for player choice
 2. Evaluate & lowercase
-3. Player choice = indexOf()
+3. Player choice = indexOf() - Moved to playRound()
 */
-function getPlayerChoice() {
-  let playerChoice = prompt("Choose rock, paper or scissors");
-  if (playerChoice) {
-    playerChoice.toLowerCase();
-    console.log(`You chose ${playerChoice}`);
-  } else {
-    playerChoice = prompt("Please enter rock, paper or scissors").toLowerCase();
-    console.log(`You chose ${playerChoice}`);
-  }
-  return CHOICES.indexOf(playerChoice);
-}
 
 /*
 Compare choices
@@ -46,63 +41,81 @@ Compare choices
 */
 function compareChoices(playerSelection, computerSelection) {
   let roundResult;
+  let roundResultDisplay = document.createElement("div");
+
   if (playerSelection === computerSelection) {
-    console.log(`It's a draw!`);
+    roundResultDisplay.textContent = `It's a draw!`;
+    results.appendChild(roundResultDisplay);
     roundResult = 2;
   } else if (
     (playerSelection === 0 && computerSelection === 1) || // rock vs. paper
     (playerSelection === 1 && computerSelection === 2) || // paper vs. scissors
     (playerSelection === 2 && computerSelection === 0) // scissors vs. rock
   ) {
-    console.log(`Computer wins this round!`);
+    roundResultDisplay.textContent = `Computer wins this round!`;
+    results.appendChild(roundResultDisplay);
     roundResult = 1;
   } else {
-    console.log(`Player wins this round!`);
+    roundResultDisplay.textContent = `Player wins this round!`;
+    results.appendChild(roundResultDisplay);
     roundResult = 0;
   }
   return roundResult;
 }
 
 /*
-Play 1 round of Rock Paper Scissors
-1. Call player choice
+REFACTORED: Play 1 round of Rock Paper Scissors
+1. Display player choice
 2. Call computer choice
-3. Call compare choices
-4. Save round results
+3. Call choices comparison & pass to progress game
 */
-function playRound() {
-  const playerChoice = getPlayerChoice();
+function playRound(choice) {
+  const playerChoice = CHOICES.indexOf(choice);
+  let playerChoiceDisplay = document.createElement("div");
+  playerChoiceDisplay.textContent = `You chose ${choice.toUpperCase()}`;
+  choicesDisplay.appendChild(playerChoiceDisplay);
   const computerChoice = getComputerChoice();
-  return compareChoices(playerChoice, computerChoice);
+  progressGame(compareChoices(playerChoice, computerChoice));
 }
 
 /*
-Match function 5 rounds
-0. declare results array
+REFACTORED: playGame => Progress game
+Count 5 points
+Moved to global: Declare results array
 Removed: Loop round function 5 times
-2. Push results to an array
-3. Evaluate array to get winner
-4. Show winner
+1. Push results to an array & reset round divs
+  if gameResults[0] < 5 or gameResults[1] < 5 {
+  2. Evaluate array to get winner
+  3. Show winner
+  4. reset game 
+  }
 */
-function playGame() {
-  const gameResults = [];
-  gameResults.push(playRound());
+function progressGame(roundResult) {
+  if (roundResult == 0) gameResults[0] += 1;
+  else if (roundResult == 1) gameResults[1] += 1;
 
-  let result = gameResults.reduce(function (points, count) {
-    if (count in points) {
-      points[count]++;
+  scoreDisplay.textContent = `Player: ${gameResults[0]} vs. Computer: ${gameResults[1]}`;
+
+  if (gameResults[0] == 5 || gameResults[1] == 5) {
+    if (gameResults[0] == 5) {
+      gameResultDisplay.textContent = `Player wins!`;
+    } else if (gameResults[1] == 5) {
+      gameResultDisplay.textContent = `Computer wins!`;
     } else {
-      points[count] = 1;
+      gameResultDisplay.textContent(`A perfect draw! Impossible...`);
     }
-    return points;
-  }, {});
 
-  if (result[1] == undefined || result[0] > result[1]) {
-    console.log(`${RESULTS[0]} wins!`);
-  } else if (result[0] == undefined || result[0] < result[1]) {
-    console.log(`${RESULTS[1]} wins!`);
-  } else {
-    console.log(`It's a draw!`);
+    scoreDisplay = `Player: ${gameResults[0]} vs. Computer: ${gameResults[1]}`;
+    choicesDisplay.replaceWith(scoreDisplay);
+    results.replaceWith(gameResultDisplay);
+    /*     controls.setAttribute("style", "pointer-events: none; opacity: 0.4;");
+    to disable buttons */
+    let btnReset = document.createElement("button");
+    btnReset.id = "reset";
+    controls.replaceChildren(btnReset);
+    btnReset.addEventListener("click", (event) => {
+      location.reload();
+    });
   }
 }
 
@@ -119,17 +132,16 @@ let controls = document.querySelector(".controls");
 controls.append(btnRock, btnPaper, btnScissors);
 
 controls.addEventListener("click", (event) => {
-  let target = event.target;
+  choicesDisplay.replaceChildren();
 
-  switch (target.id) {
-    case "rock":
-      console.log("Rock control was clicked");
-      break;
-    case "paper":
-      console.log("Paper control was clicked");
-      break;
-    case "scissors":
-      console.log("Scissors control was clicked");
-      break;
+  if (results.hasChildNodes()) {
+    results.replaceChildren(scoreDisplay);
   }
+  let target = event.target;
+  playRound(target.id);
 });
+
+let results = document.querySelector(".results");
+let choicesDisplay = document.querySelector(".choices");
+let gameResultDisplay = document.createElement("div");
+let scoreDisplay = document.createElement("div");
